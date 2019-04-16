74d9f9e64d16af54e9ceb4d876bd1e49568698e7
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-15 20:12:07 -0600

    Commit 5 - added dynamic anchor tags to index.ejs

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const pokemon = require('./pokemon');
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/pokemon', (req, res) => {
+	res.render('index.ejs', {pokemon: pokemon});
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  res.render('show.ejs', {pokemon: pokemon[req.params.id]});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function() {
+  console.log("Server is listening on port", port);
+});
